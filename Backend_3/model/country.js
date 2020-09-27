302785ff05fe7dfbf081609a01a831f242f69a01
dicky08 <dickyf147@gmail.com>
2020-09-27 09:19:09 +0700

    delete users

--- Backend_3/model/country.js
+++ Backend_3/model/country.js
@@ -1,15 +1,42 @@
-const db = require('../config/database')
+const db = require('.././config/database')
 
-module.exports = {
-    getAll: (req,res) => {
-        return new Promise((resolve, reject) => {
-            db.query('SELECT * from country', (err, result)=> {
-                if(err) {
-                   reject(err)
-                } else {
-                    resolve(result)
-                }
+const country = {
+    getAll: () => {
+        return new Promise((resolve,reject)=>{
+            db.query(`SELECT * from country`,(err,result)=>{
+                err?reject(new Error(err)):resolve(result)
+            })
+        })
+    },
+    add: (data) => {
+        return new Promise((resolve,reject)=>{
+            db.query(`INSERT into country (id_country,code_country,name_country)VALUES('${data.id_country}','${data.code_country}','${data.name_country}')`,(err,result)=>{
+                err? reject(new Error(err)) :resolve(result)
+            })
+        })
+    },
+    getDetail: (id) => {
+        return new Promise((resolve,reject)=> {
+            db.query(`SELECT * from country where id_country = ${id} `,(err,result)=>{
+                err?reject(new Error(err)):resolve(result)
+            })
+        })
+    },
+    update: (data,id) => {
+        return new Promise((resolve,reject)=>{
+            db.query(`UPDATE country set code_country = '${data.code_country}',
+            name_country = '${data.name_country}' where id_country = '${id}'`,(err,result)=>{
+                err?reject(new Error(err)):resolve(result)
+            })
+        })
+    },
+    delete: (id) => {
+        return new Promise((resolve,reject)=> {
+            db.query(`DELETE from country where id_country = ${id}`,(err,result)=>{
+                err?reject(new Error(err)):resolve(result)
             })
         })
     }
 }
+
+module.exports = country
