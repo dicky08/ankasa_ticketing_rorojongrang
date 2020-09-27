ce6e43c8ecbb6afb5776d3d0b291bfda70c6a70e
sofyan-dev <mchsfyan17@gmail.com>
2020-09-28 06:27:07 +0700

    new

--- Backend_3/model/airlines.js
+++ Backend_3/model/airlines.js
@@ -3,7 +3,16 @@ const db = require('.././config/database')
 const airlines = { 
     dataAll: () => {
         return new Promise((resolve,reject)=> {
-            db.query(`SELECT * from airlines `,(err,result)=>{
+            db.query(`SELECT airlines.id_airlines,airlines.code_airlines ,
+             airlines.name_airlines,
+            airlines.price,
+            airlines.image_airlines,
+            airlines.child,
+            airlines.adult,
+            airlines.type,
+            airlines.departure_day
+            from airlines`
+            ,(err,result)=>{
                 if(err){
                     reject(new Error(err))
                 }else{
@@ -19,9 +28,39 @@ const airlines = {
             })
         })
     },
-    addData: (body) => {
+    addData: (data) => {
         return new Promise((resolve,reject)=>{
-            db.query(`INSERT into airlines SET ?`,body,(err,result)=>{
+          /*  db.query(`INSERT into airlines 
+            (id_airlines,
+            code_airlines,
+            name_airlines,
+            price,
+            image_airlines,
+            child,
+            adult,
+            type,
+            departure_day,
+            rating,
+            id_transit,
+            id_facilities,
+            id_departure_time,
+            id_time_arrived)values
+            ('${data.id_airlines}',
+            '${data.code_airlines}',
+            '${data.name_airlines}',
+            '${data.price}',
+            '${data.image_airlines}',
+            '${data.child}',
+            '${data.adult}',
+            '${data.type}',
+            '${data.departure_day}',
+            '${data.rating}',
+            '${data.id_transit}',
+            '${data.id_facilities}',
+            '${data.id_departure_time}',
+            '${data.id_time_arrived}')`
+            */
+            db.query(`insert into airlines set ?`,data,(err,result)=>{
                 err? reject(new Error(err)) :resolve(result)
             })
         })
