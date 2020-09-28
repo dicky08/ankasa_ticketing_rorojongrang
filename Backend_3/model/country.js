f69dc8d01c9acd90a0a8178c301e5396bc5cfb79
farhandz <ammarjoz09@gmail.com>
2020-09-28 09:36:42 +0700

    add country

--- Backend_3/model/country.js
+++ Backend_3/model/country.js
@@ -1,42 +1,54 @@
-const db = require('.././config/database')
+const db = require(".././config/database");
 
 const country = {
-    getAll: () => {
-        return new Promise((resolve,reject)=>{
-            db.query(`SELECT * from country`,(err,result)=>{
-                err?reject(new Error(err)):resolve(result)
-            })
-        })
-    },
-    add: (data) => {
-        return new Promise((resolve,reject)=>{
-            db.query(`INSERT into country (id_country,code_country,name_country)VALUES('${data.id_country}','${data.code_country}','${data.name_country}')`,(err,result)=>{
-                err? reject(new Error(err)) :resolve(result)
-            })
-        })
-    },
-    getDetail: (id) => {
-        return new Promise((resolve,reject)=> {
-            db.query(`SELECT * from country where id_country = ${id} `,(err,result)=>{
-                err?reject(new Error(err)):resolve(result)
-            })
-        })
-    },
-    update: (data,id) => {
-        return new Promise((resolve,reject)=>{
-            db.query(`UPDATE country set code_country = '${data.code_country}',
-            name_country = '${data.name_country}' where id_country = '${id}'`,(err,result)=>{
-                err?reject(new Error(err)):resolve(result)
-            })
-        })
-    },
-    delete: (id) => {
-        return new Promise((resolve,reject)=> {
-            db.query(`DELETE from country where id_country = ${id}`,(err,result)=>{
-                err?reject(new Error(err)):resolve(result)
-            })
-        })
-    }
-}
+  getAll: () => {
+    return new Promise((resolve, reject) => {
+      db.query(`SELECT * from country`, (err, result) => {
+        err ? reject(new Error(err)) : resolve(result);
+      });
+    });
+  },
+  add: (data) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `INSERT into country (id_country,code_country,name_country)VALUES('${data.id_country}','${data.code_country}','${data.name_country}')`,
+        (err, result) => {
+          err ? reject(new Error(err)) : resolve(result);
+        }
+      );
+    });
+  },
+  getDetail: (id) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `SELECT * from country where id_country = ${id} `,
+        (err, result) => {
+          err ? reject(new Error(err)) : resolve(result);
+        }
+      );
+    });
+  },
+  update: (data, id) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `UPDATE country set code_country = '${data.code_country}',
+            name_country = '${data.name_country}' where id_country = '${id}'`,
+        (err, result) => {
+          err ? reject(new Error(err)) : resolve(result);
+        }
+      );
+    });
+  },
+  delete: (id) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `DELETE from country where id_country = ${id}`,
+        (err, result) => {
+          err ? reject(new Error(err)) : resolve(result);
+        }
+      );
+    });
+  },
+};
 
-module.exports = country
+module.exports = country;
